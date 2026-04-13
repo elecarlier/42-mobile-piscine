fad65a11b7c543e69ea2b5e4a3c3d648c4d9b390
elecarlier <eleonorecarlier@hotmail.be>
2026-04-13 12:38:01 +0200

    spo

--- mobile-module-00/app/ex02.tsx
+++ mobile-module-00/app/ex02.tsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { Appbar } from 'react-native-paper';
 
 export default function Ex02() {
-  const [isPressed, setPressed] = useState(false);
-
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{isPressed ? "Hello World!" : "I love tractors"}</Text>
-      <Button title="Me too" onPress={() => setPressed(!isPressed)} />
+    <View style={{ flex: 1 }}>        {/* ← le "un seul truc" */}
+      <Appbar.Header>
+        <Appbar.Content title="Calculator" />
+      </Appbar.Header>
+      <TextInput value="0" />
+      <TextInput value="0" />
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity style={{ flex: 1 }}>
+          <Text>7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ flex: 1 }}>
+          <Text>8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ flex: 1 }}>
+          <Text>9</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ flex: 1 }}>
+          <Text>9</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
